82e6d360cd337e6cc2474504cfb9828bb9dfbfdf
Anthony Alexandrov <ana524@lehigh.edu>
2020-08-26 23:31:26 -0400

    edited

--- Intro to R.R
+++ Intro to R.R
@@ -6,4 +6,4 @@
 # in your script by putting "#" at the beginning of the lines.
 # 
 # In fact, you can put comments anywhere on a line as long as 
-# you put "#" before them.
+# you put "#" before them
